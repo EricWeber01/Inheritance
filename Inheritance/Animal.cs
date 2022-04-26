c710694f014a0fa2edcffd7c830a9b0833f69553
babya <babya@DESKTOP-6H8S8H4>
2022-04-26 13:37:47 +0300

    Add project files.

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Wight { get; set; }
+        protected Animal(string _n, string _c, int _w)
+        {
+            Name = _n;
+            Color = _c;
+            Wight = _w;
+        }
+    }
+    class Predator : Animal
+    {
+        string type;
+        public Predator(string _n, string _c, int _w) : base(_n, _c, _w)
+        {
+            type = "Хищник";
+        }
+        public override string ToString()
+        {
+            return $"Имя: {this.Name} | Вес: {this.Wight} | Цвет: {this.Color} | Тип: {this.type}\n";
+        }
+        public void Run(Victim v)
+        {
+            Console.WriteLine($"{this.Name} хочет сьесть {v.Name}\n");
+        }
+    }
+    class Victim : Animal
+    {
+        string type;
+        public Victim(string _n, string _c, int _w) : base(_n, _c, _w)
+        {
+            type = "Жертва";
+        }
+        public override string ToString()
+        {
+            return $"Имя: {this.Name} | Вес: {this.Wight} | Цвет: {this.Color} | Тип: {this.type}\n";
+        }
+        public void Run(Predator p)
+        {
+            Console.WriteLine($"{this.Name} убегает от {p.Name}\n");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Predator dog = new Predator("Собака", "Черный", 7);
+            Predator cat = new Predator("Кот", "Рыжий", 4);
+            Victim mouse = new Victim("Мышь", "Серый", 1);
+            Victim rabbit = new Victim("Кролик", "Белый", 2);
+            Console.WriteLine(dog);
+            Console.WriteLine(cat);
+            Console.WriteLine(mouse);
+            Console.WriteLine(rabbit);
+            cat.Run(mouse);
+            rabbit.Run(dog);*/
+
+            MyDate d = new MyDate(27, 03, 2022);
+            Console.WriteLine(d);
+            bool check = d.IfLeap();
+            check = d.IfToDay();
+            d++;
+            Console.WriteLine(d);
+            check = d.IfToDay();
+        }
+    }
+}
